8f80506fbfe55ee302a390016cb898449672f894
elianahj <kity3399@naver.com>
2022-04-25 19:44:09 +0900

    commit

--- src/FordFulkerson.java
+++ src/FordFulkerson.java
@@ -3,52 +3,55 @@
 import java.util.*;
 
 public class FordFulkerson {
-                              //[][]R = 잔여 용량, src = 출발점, s = 도착점, path = 출발점->도착점 가는 경로 저장
-    private static boolean bfs(int[][] R, int src, int s, int[] path) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[R.length];
+
+    // 1.경로 찾기
+    //residual = 잔여 용량, src = 출발점, s = 도착점, path = 출발점->도착점 가는 경로 저장
+    private static boolean findpath(int[][] r, int src, int s, int[] path) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[r.length]; //정점을 방문했었는지 여부 저장
         queue.add(src);
         visited[src]=true;
 
         while(!queue.isEmpty()) {
-            int a = queue.poll(); //a = 현재 노드
-            for(int i=0;i<R.length; i++){
-                if(visited[i]) continue;
-                if(R[a][i]>0){ //잔여 용량이 더 크다면 방문합니다
+            int a = queue.poll(); //a = 현재 정점
+            for(int i=0;i<r.length; i++){
+                if(!visited[i] && r[a][i]>0){ //방문하지 않은 정점 중에서 잔여 용량이 남아있으면 방문합니다
                     queue.add(i);
                     visited[i] = true;
                     path[i] = a; //i를 현재 노드로 설정하고 다시 반복
-                    if (i==s) return true; //도착지에 도착하면 리턴
+                    if (i==s) return true; //도착지에 도착할 때까지
                 }
             }
         }
         return false;
     }
 
+    //2.Ford Fulkerson 알고리즘 수행
     private static int fordfulkerson(int[][] graph, int source, int sink) {
         int max = 0; //max = 최대 용량
-        int[][] R = new int[graph.length][graph.length]; //잔여 용량
-        for(int i=0;i< graph.length;++i){
-            System.arraycopy(graph[i],0,R[i],0,graph.length);
+        int[][] r = new int[graph.length][graph.length]; //잔여 용량 residual
+        for (int i = 0; i < graph.length; ++i) {
+            System.arraycopy(graph[i], 0, r[i], 0, graph.length); //최초의 잔여 용량은 보낸 용량이 없으므로 생성한 그래프와 같으니까 복붙해줍니다
         }
         int[] path = new int[graph.length];
 
-        while (bfs(R, source, sink, path)) { //bfs로 찾은 경로들에 대해 동시에 보낼 수 있는 최대 용량을 찾습니다
+        while (findpath(r, source, sink, path)) { //찾은 경로들에 대해 동시에 보낼 수 있는 최대 용량을 찾습니다
             int F = Integer.MAX_VALUE;
-            int a = sink;
-            while(a!=source) {
-                int parent = path[a];
-                F = Math.min(F,R[parent][a]); //경로 중 잔여 용량이 더 작은 것을 찾는다
-                a = parent;
-            }
-            max += F; //최대 용량에 더해줌
+            int i = sink;
 
-            a = sink;
-            while (a!=source){
-                int parent = path[a];
-                R[parent][a] -= F;//정간선에는 잔여 용량에 보내는 용량을 빼주고
-                R[a][parent] += F;//역간선에는 잔여 용량에 보내는 용량을 더해준다
-                a = parent;
+            while (i!=source) {
+                int parent = path[i];
+                F = Math.min(F,r[parent][i]); //경로 내 최소값을 찾아서 그만큼의 용량을 보낸다
+                i = parent;
+            }
+            max += F; //보낸 용량을 최대 용량에 더해줌
+
+            i=sink;
+            while(i!=source){
+                int parent = path[i];
+                r[parent][i] -= F;//정간선에는 잔여 용량에 보내는 용량을 빼주고
+                r[i][parent] += F;//역간선에는 잔여 용량에 보내는 용량을 더해준다
+                i = parent;
             }
         }
         return max;
